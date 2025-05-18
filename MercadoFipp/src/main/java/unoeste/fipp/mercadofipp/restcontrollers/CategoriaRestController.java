8bb0a3381ee1faf3ac03077d673b9b2ae47a08d0
LeandroPaes2 <lelepaes205@gmail.com>
2025-05-18 14:32:10 -0300

    Primeiro commit

---
+++ MercadoFipp/src/main/java/unoeste/fipp/mercadofipp/restcontrollers/CategoriaRestController.java
@@ -0,0 +1,47 @@
+package unoeste.fipp.mercadofipp.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import unoeste.fipp.mercadofipp.entities.Categoria;
+import unoeste.fipp.mercadofipp.entities.Erro;
+import unoeste.fipp.mercadofipp.services.CategoriaService;
+
+import java.util.List;
+@CrossOrigin
+@RestController
+@RequestMapping("apis/categoria")
+public class CategoriaRestController {
+    @Autowired
+    private CategoriaService categoriaService;
+    @GetMapping
+    public ResponseEntity<Object> getAll(){
+        List<Categoria> categoriaList = categoriaService.getAll();
+        if(categoriaList!=null && !categoriaList.isEmpty())
+            return ResponseEntity.ok(categoriaList);
+        return ResponseEntity.badRequest().body(new Erro("categorias não encontradas"));
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Object> getId(@PathVariable int id){
+        Categoria categoria = categoriaService.getId((long)id);
+        if(categoria!=null)
+            return ResponseEntity.ok(categoria);
+        return ResponseEntity.badRequest().body(new Erro("categoria não encontradas"));
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> delete(@PathVariable int id){
+        if(categoriaService.delete((long)id))
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.badRequest().body(new Erro("categoria ao deletar a categoria"));
+    }
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody Categoria categoria){
+        Categoria novaCategoria=categoriaService.save(categoria);
+        if(categoria!=null)
+            return ResponseEntity.ok(categoria);
+        return ResponseEntity.badRequest().body("Erro ao cadastrar a categoria");
+    }
+    // alterar
+    //getId
+    //apagar
+}
---
+++ MercadoFipp/src/main/java/unoeste/fipp/mercadofipp/services/CategoriaService.java
@@ -0,0 +1,37 @@
+package unoeste.fipp.mercadofipp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import unoeste.fipp.mercadofipp.entities.Categoria;
+import unoeste.fipp.mercadofipp.repositories.CategoriaRepository;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    public List<Categoria> getAll(){
+        return categoriaRepository.findAll();
+    }
+    public Categoria save(Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria getId(Long id) {
+        return categoriaRepository.findById(id).get();
+    }
+
+    public boolean delete(long id) {
+        try {
+            categoriaRepository.delete(new Categoria(id, ""));
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+    //inserir/alterar
+    //getId
+    //apagar
+}
